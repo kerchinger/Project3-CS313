d9ee18587489ff8a55a988bf8d5719ff712eac94
Grazyna Choinski <gchoinski@luc.edu>
2016-10-16 14:11:16 -0500

    I did some changes in BoundingBox. TestBoundingBox passes.

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/BoundingBox.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/BoundingBox.java
@@ -10,8 +10,6 @@ import java.util.List;
  */
 public class BoundingBox implements Visitor<Location> {
 
-	// TODO entirely your job (except onCircle)
-
 	@Override
 	public Location onCircle(final Circle c) {
 		final int radius = c.getRadius();
@@ -19,42 +17,58 @@ public class BoundingBox implements Visitor<Location> {
 	}
 
 	@Override
-	public Location onFill(final Fill f) {return new Location(0,0,f.getShape());} // passes TestBoundingBox
+	public Location onFill(final Fill f) {return f.getShape().accept(this);}
 
 	@Override
 	public Location onGroup(final Group g) {
-		int x1 = 0;
-		int y1 = 0;
-		Shape shape1 = new Rectangle(0,0);
-		Iterator i = g.getShapes().iterator();
-		while(i.hasNext()) {
-			Location location =(Location)((Shape) i.next()).accept(this);
-			x1 = location.getX();
-			y1 = location.getY();
-			shape1 = location.getShape();
+		int xMin = Integer.MAX_VALUE;
+		int xMax = Integer.MIN_VALUE;
+		int yMin = Integer.MAX_VALUE;
+		int yMax = Integer.MIN_VALUE;
+		Iterator<? extends Shape> i = g.getShapes().iterator();
+		while (i.hasNext()) {
+			Location locationStart = (Location)i.next().accept(this);//gets boundingbox of shape
+			Rectangle shapeBoundingBox = (Rectangle)locationStart.getShape();//gets rectangle from the boundingbox
+			xMin = Math.min(xMin, locationStart.getX());
+			yMin = Math.min(yMin, locationStart.getY());
+			xMax = Math.max(xMax, locationStart.getX() + shapeBoundingBox.getWidth());
+			yMax = Math.max(yMax, locationStart.getY() + shapeBoundingBox.getHeight());
 		}
-		return new Location(x1, y1, shape1); // TODO this doesn't work
+		return new Location (xMin, yMin, new Rectangle(xMax - xMin, yMax - yMin));
 	}
 
 	@Override
-	public Location onLocation(final Location l) {return new Location(l.getX(), l.getY(), l.getShape());} // passes TestBpundingBox
+	public Location onLocation(final Location l) {
+		Location shapeBoundingBox = l.getShape().accept(this);
+		return new Location(l.getX() + shapeBoundingBox.getX(), l.getY() + shapeBoundingBox.getY(), shapeBoundingBox.getShape());//location must be corrected by existing location
+	}
 
 	@Override
-	public Location onRectangle(final Rectangle r) {return new Location(0, 0, r);}// passes TestBpundingBox
+	public Location onRectangle(final Rectangle r) {
+		return new Location(0, 0, new Rectangle(r.getWidth(), r.getHeight()));}// passes TestBpundingBox
 
 	@Override
-	public Location onStroke(final Stroke c) {return new Location(0,0,c.getShape());}// passes TestBpundingBox I don't kow how accurate the code is
+	public Location onStroke(final Stroke c) {return c.getShape().accept(this);}// changed like in Size
 
 	@Override
 	public Location onOutline(final Outline o) {
-		return new Location(0,0,o.getShape());
-	}// passes TestBpundingBox but I would look back to make sure this is correct
+		return o.getShape().accept(this);// changed it like in the Size
+	}
 
 	@Override
 	public Location onPolygon(final Polygon s) {
-		// still need to work on this so that it runs properly
-		final List<? extends Point> points = s.getPoints();
-		points.get(0);
-		return new Location(50,50, new Rectangle(70, 60)); //TODO this deosn't work
+		int xl = Integer.MAX_VALUE;
+		int xr = Integer.MIN_VALUE;
+		int yd = Integer.MAX_VALUE;
+		int yu = Integer.MIN_VALUE;
+		Iterator<? extends Point> i = s.getPoints().iterator();
+		while (i.hasNext()) {
+			Point polygonPoint = i.next();
+			xl = Math.min(xl, polygonPoint.getX());
+			xr = Math.max(xr, polygonPoint.getX());
+			yd = Math.min(yd, polygonPoint.getY());
+			yu = Math.max(yu, polygonPoint.getY());
+		}
+		return new Location (xl, yd, new Rectangle(xr-xl, yu-yd));
 	}
 }
