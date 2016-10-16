69120eb5c1a071a896d11f9c40c7d16a67835c3d
Kyle Erchinger <kerchinger@luc.edu>
2016-10-16 17:56:05 -0500

    So, i fixed all the Draw methods so that they find what height and width is by themselves. And I got onLocation Test to work, but I'm having some trouble figuring out the onGroup method. I have it started, and I have a comment where its breaking.

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
@@ -3,6 +3,11 @@ package edu.luc.etl.cs313.android.shapes.android;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
+import android.graphics.Rect;
+import android.graphics.RectF;
+
+import java.util.Iterator;
+
 import edu.luc.etl.cs313.android.shapes.model.*;
 
 /**
@@ -30,40 +35,67 @@ public class Draw implements Visitor<Void> {
 
 	@Override
 	public Void onStroke(final Stroke c) { // it works and draws a red outlines rectangle
+		Rectangle r1 = new Rectangle(0,0);
+		r1 = (Rectangle) c.getShape();
 		paint.setColor(c.getColor());
-		canvas.drawRect(0,0, 80, 120, paint);
+		canvas.drawRect(0,0, r1.getWidth(), r1.getHeight(), paint);
 		return null;
 	}
 
 	@Override
 	public Void onFill(final Fill f) { // it fills a rectangle with a solid color
+		Rectangle r2 = null;
+		r2 = (Rectangle) f.getShape();
 		paint.setStyle(Style.FILL);
-		canvas.drawRect(0,0,50, 100, paint);
+		canvas.drawRect(0,0,r2.getHeight(), r2.getWidth(), paint);
 		return null;
 	}
 
 	@Override
 	public Void onGroup(final Group g) {
+		Object shapequestion;
+		Rectangle rect = new Rectangle(0,0);
+		Circle circ = new Circle(0);
+		Iterator<? extends Shape> i = g.getShapes().iterator();
 
+		while (i.hasNext()){
+		Location locationt = (Location) i.next();
+			canvas.translate(locationt.getX(), locationt.getY());
+			shapequestion = locationt.getShape();
+
+			if(shapequestion.equals(rect)){ // problem is right here, how to check whether or not shape is a rectangle or not
+				rect = (Rectangle) shapequestion;
+				canvas.drawRect(0,0,rect.getWidth(), rect.getHeight(),paint);
+			} else {
+				circ = (Circle) shapequestion;
+				canvas.drawCircle(0,0,circ.getRadius(), paint);
+			}
+			canvas.translate(-locationt.getX(), - locationt.getY());
+		}
 		return null;
 	}
 
 	@Override
-	public Void onLocation(final Location l) {
-		canvas.translate(0,0); // moves origin
-		canvas.drawRect(0,0,l.getY(),l.getX(), paint); // doesn't pass
+	public Void onLocation(final Location l) { // works!!
+		Rectangle r = new Rectangle(0,0);
+		r = (Rectangle) l.getShape();
+		canvas.translate(l.getX(),l.getY()); // moves origin
+		canvas.drawRect(0,0,r.getWidth(),r.getHeight(),paint);
+		canvas.translate(- l.getX(), - l.getY());
 		return null;
 	}
 
 	@Override
 	public Void onRectangle(final Rectangle r) {
-		canvas.drawRect(0,0,100, 150, paint);
+		canvas.drawRect(0,0,r.getWidth(), r.getHeight(), paint);
 		return null;
 	}
 
 	@Override
 	public Void onOutline(Outline o) {
-	canvas.drawRect(0,0,0,0,paint);
+		Rectangle r3 = null;
+		r3 = (Rectangle) o.getShape();
+	canvas.drawRect(0,0,r3.getHeight(),r3.getWidth(),paint);
 		return null;
 	}
 
